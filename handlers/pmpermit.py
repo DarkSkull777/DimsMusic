2b77116294fce1ebaf97b15291b933f0c1e5542b
Boncel-Cell <53797562+Boncel-Cell@users.noreply.github.com>
2021-05-26 05:02:42 +0800

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n")
+  await USER.send_message(message.chat.id,"Halo, Ini adalah layanan asisten musik.\n\n ❗️ Rules:\n   - tidak ada mengobrol yang diizinkan\n   - tidak ada spam yang diizinkan \n\n 👉 **KIRIM LINK UNDANGAN GRUP ATAU NAMA PENGGUNA JIKA USERBOT TIDAK DAPAT BERGABUNG DENGAN GRUP ANDA.**\n\n ⚠️ Peringatan: Jika Anda mengirim pesan di sini berarti admin akan melihat pesan Anda dan bergabung dengan obrolan\n    - Jangan tambahkan pengguna ini ke grup rahasia.\n   - Jangan Bagikan info pribadi di sini\n\n")
   return                        
