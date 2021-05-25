a90d908f8e77bacfb47999d36d5e4a1d029d193b
Boncel-Cell <53797562+Boncel-Cell@users.noreply.github.com>
2021-05-26 05:29:24 +0800

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,25 +9,31 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @W2HSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Haii, My Name is  {bn}\n\n Saya Bot Music Group, Yang dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah
+Saya Memiliki Banyak Fitur Praktis Seperti :\n **┏━━━━━━━━━━━━━━
+┣• Memutar Musik.
+┣• Mendownload Lagu.
+┣• Mencari Lagu Yang ingin di Putar atau di Download.
+┗━━━━━━━━━━━━━━** \n **❃ Managed With ☕️ By : @dims_whydi** \n **━━━━━━━━━━━━━━━** \n\n **Asisten harus ada di grup Anda untuk memutar musik di obrolan suara grup Anda.** \n\ **untuk mengetahui perintah saya tekan /help**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/W2HSupport"
+                        "OWNER⚡️", url="https://t.me/dims_whydi"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/David99q"
+                        "JOIN GROUP⚡️", url="https://t.me/CARI_DOI_INDONESIA"
                     ),
                     InlineKeyboardButton(
-                        "Ravan102030⚡️", url="https://t.me/Ravan102030"
+                        "JOIN CHANNEL⚡️", url="https://t.me/GOLDEN_QUOTES_INDONESIA
+"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Add To Your Group⚡️", url="https://t.me/W2H_Music_1_Bot?startgroup=true"
+                        "Tambahkan Ke Group⚡️", url="https://t.me/@MusicDimsBot?startgroup=true"
                     )
                 ]
             ]
